421f5cac9f21d6255b1acd5e297e808cc86dc5e1
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-12-03 19:05:40 +0100

    Puzzle #1

---
+++ aoc2022-1-part2.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use loophp\collection\Collection;
+
+include __DIR__ . '/vendor/autoload.php';
+
+$input = trim(file_get_contents(__DIR__ . '/input.txt'));
+
+$c = Collection::fromString($input, "\n\n")
+    ->map(static fn (string $lines): array => explode("\n", $lines))
+    ->map(static fn (array $lines): int => array_sum($lines))
+    ->sort()
+    ->reverse()
+    ->chunk(3)
+    ->map(
+        static fn (array $lines): int => array_sum($lines)
+    )
+    ->all();
+
+dump($c);
