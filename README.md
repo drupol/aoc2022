9d3f59eda2bd7340f825df24da418f21bc8cc4ca
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-12-06 06:14:44 +0100

    Puzzle #6

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ You don't need a computer science background to participate - just a little prog
 - [x] Day 3
 - [x] Day 4
 - [x] Day 5
-- [ ] Day 6
+- [x] Day 6
 - [ ] Day 7
 - [ ] Day 8
 - [ ] Day 9
---
+++ aoc2022-6-part2.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Generator;
+use loophp\collection\Collection;
+use SplStack;
+
+include __DIR__ . '/vendor/autoload.php';
+
+$input = trim(file_get_contents(__DIR__ . '/input.txt'));
+
+$size = 14;
+
+$c = Collection::fromString($input)
+    ->window($size - 1)
+    ->until(
+        static fn (array $item): bool => $item === array_flip(array_flip($item))
+    )
+    ->flip();
+
+print_r($c->last() + 1);
